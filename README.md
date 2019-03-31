41c1027ccc25d5f67708e2318fbf0aaed5accb64
Jacob Williams <jacobwilliams@users.noreply.github.com>
2019-03-30 23:38:25 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,65 @@
+Just some experiments using Python multiprocessing to read many namelists in parallel.
+
+### Results
+
+```
+-----------------------------
+ reading it all at once from file:
+-----------------------------
+
+6.814043045043945 sec
+
+-----------------------------
+ reading it all at once from string:
+-----------------------------
+
+6.754244089126587 sec
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 1 threads.
+2.543579339981079 sec
+
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 2 threads.
+1.4157969951629639 sec
+
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 3 threads.
+0.9900557994842529 sec
+
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 4 threads.
+0.8005549907684326 sec
+
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 5 threads.
+0.8011331558227539 sec
+
+
+-----------------------------
+ reading it in parallel:
+-----------------------------
+
+using 6 threads.
+0.8105840682983398 sec
+
+```
---
+++ fast_namelist.py
@@ -0,0 +1,132 @@
+
+import f90nml
+import multiprocessing as mp
+import time
+
+#####################################
+def read_a_namelist(str,parser):
+    #nml = parser.reads(str)          # ... not in official release yet ...
+    nml = parser._readstream(iter(str.splitlines()),{})
+    return nml
+
+#####################################
+def split_namelist(filename):
+    namelists = []
+    i = -1
+    started=False
+    with open(filename,'r') as f:
+        for line in f:
+            line = line.strip()
+            if (len(line)>0):
+                if (line[0:1]!='!'):
+                    if (line[0:1]=='&'): # start a namelist
+                        i = i + 1
+                        namelists.append('')
+                        started = True
+                    elif (line[0:1]=='/'): # end a namelist
+                        started = False
+                        namelists[i] = namelists[i] + line + '\n'
+                        continue
+                    if started:
+                        namelists[i] = namelists[i] + line + '\n'
+    return namelists
+
+#####################################
+def read_namelist_fast(filename,n_threads=4):
+
+    parser = f90nml.Parser()
+    parser.global_start_index = 1  # need this for my use cases
+
+    namelists = split_namelist(filename)
+
+    n_threads = max(1,min(mp.cpu_count(),n_threads))
+    print('using '+str(n_threads)+' threads.')
+    pool = mp.Pool(processes=n_threads)
+    results = []
+    for s in namelists:
+        results.append(pool.apply_async(read_a_namelist,(s,parser)))
+    pool.close()
+    pool.join()
+
+    # create a single namelist from the results:
+    nml = f90nml.Namelist({})
+    for r in results:
+        for key, value in r.get().items():
+            if key in nml:
+                # array of namelists:
+                if isinstance(nml[key],list):
+                    nml[key].append(value)
+                else:
+                    nml[key] = [nml[key], value]
+            else:
+                nml[key] = value
+
+    return nml
+
+########################################################################
+if __name__ == "__main__":
+
+    """ test case """
+
+    filename = 'files/test.nml'
+
+    ##################
+
+    print('')
+    print('-----------------------------')
+    print(' reading it all at once from file:')
+    print('-----------------------------')
+    print('')
+    start_time = time.time()
+
+    p = f90nml.Parser()
+    p.global_start_index = 1
+    nml = p.read(filename)
+
+    end_time = time.time()
+    print(str(end_time-start_time) + ' sec')
+
+    # print('')
+    # print(nml)
+
+    ##################
+
+
+    print('')
+    print('-----------------------------')
+    print(' reading it all at once from string:')
+    print('-----------------------------')
+    print('')
+    start_time = time.time()
+
+    p = f90nml.Parser()
+    p.global_start_index = 1
+    with open(filename,'r') as f:
+        s = f.read()
+        nml = p._readstream(iter(s.splitlines()),{})
+
+    end_time = time.time()
+    print(str(end_time-start_time) + ' sec')
+
+    # print('')
+    # print(nml)
+
+    ##################
+
+    for n_threads in range(1,7):
+
+        print('')
+        print('-----------------------------')
+        print(' reading it in parallel:')
+        print('-----------------------------')
+        print('')
+        start_time = time.time()
+
+        res = read_namelist_fast(filename, n_threads=n_threads)
+        end_time = time.time()
+        print(str(end_time-start_time) + ' sec')
+
+        # print('')
+        # print(nml)
+
+        print('')
